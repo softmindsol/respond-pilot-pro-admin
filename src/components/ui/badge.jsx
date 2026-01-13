3a6e874fe66625cde33ab17cf5ab59c2eb45a5f7
Huzaifa-Solutions <huzaifaihsan002@gmail.com>
2026-01-13 12:15:00 +0500

    style: update inactive status color to tertiary and add new tertiary badge variant for improved visual distinction

--- src/components/ui/badge.jsx
+++ src/components/ui/badge.jsx
@@ -11,9 +11,11 @@ const badgeVariants = cva(
         default:
           "border-transparent bg-[#6f097a] text-primary-foreground [a&]:hover:bg-primary/90",
         secondary:
-          "border-transparent bg-[#1e86e8] text-white [a&]:hover:bg-secondary/90",
+          "border-transparent bg-[#1e86e899] text-white [a&]:hover:bg-secondary/90",
+        tertiary:
+          "border-transparent bg-light/50 text-white font-semibold",
         destructive:
-          "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "border-transparent bg-red/50 text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "text-white [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
         success: "border-transparent bg-green-500/20 text-green-500",
